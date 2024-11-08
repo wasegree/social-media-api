9c94192ee78b516ce717ddde0925f84f923de956
wasegree <williamsegree@gmail.com>
2024-11-08 16:32:40 -0500

    ceated user model

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    bio = models.TextField()
+    online_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
