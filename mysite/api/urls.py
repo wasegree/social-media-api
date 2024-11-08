8b067803cab9a50401cd71d65e32478420655c8d
wasegree <williamsegree@gmail.com>
2024-11-08 17:55:25 -0500

    added a retrieve, update, and delete view

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("user/", views.UserProfileListCreate.as_view(), name="UserProfileListCreate")
+    path("user/", views.UserProfileListCreate.as_view(), name="UserProfileListCreate"),
+    path(
+        "user/<int:pk>",
+        views.UserProfileRetrieveUpdateDestroy.as_view(),
+        name="UserProfileRetrieveUpdateDestroy",
+    ),
 ]
